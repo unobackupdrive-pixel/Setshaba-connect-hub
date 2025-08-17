cfa94a9521c99818f5289f07f41a24ca4fd99492
unobackupdrive-pixel <unobackupdrive@gmail.com>
2025-08-17 03:59:24 +0200

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Stack } from 'expo-router';
 import { AppProvider } from '../src/context/AppContext';
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
   return (
     <AppProvider>
       <StatusBar style="dark" />
